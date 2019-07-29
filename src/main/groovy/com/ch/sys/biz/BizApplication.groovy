2f4fa93633571374a509a0d4c99a95acab9ceaec
itmanharry <jack_chengqian@163.com>
2019-07-29 16:44:23 +0800

    redis cache for mybatis

--- src/main/groovy/com/ch/sys/biz/BizApplication.groovy
+++ src/main/groovy/com/ch/sys/biz/BizApplication.groovy
@@ -1,9 +1,11 @@
 package com.ch.sys.biz
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 import com.ch.sys.biz.BizApplication
+import com.ch.sys.biz.system.utils.SpringContextUtils
 /**
  * 工程启动
  * @author 20112004
@@ -15,7 +17,8 @@ import com.ch.sys.biz.BizApplication
 class BizApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run(BizApplication, args)
+		ConfigurableApplicationContext context = SpringApplication.run(BizApplication, args)
+		SpringContextUtils.setApplicationContext(context)
 	}
 
 }
