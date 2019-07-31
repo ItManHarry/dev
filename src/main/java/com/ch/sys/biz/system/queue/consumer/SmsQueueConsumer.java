5ed56674bd87eebc88fd80e5fe5b17db17d5f5be
itmanharry <jack_chengqian@163.com>
2019-07-31 10:30:32 +0800

    sms

---
+++ src/main/java/com/ch/sys/biz/system/queue/consumer/SmsQueueConsumer.java
@@ -0,0 +1,14 @@
+package com.ch.sys.biz.system.queue.consumer;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import com.ch.sys.biz.system.configuration.rabbitmq.RabbitMQFanoutConfig;
+@Component
+@RabbitListener(queues = RabbitMQFanoutConfig.QUEUE_SMS)
+public class SmsQueueConsumer {
+
+	@RabbitHandler
+	public void doSmsConsumer(String message) {
+		System.out.println("==============>SMS Queue Has Been Consumed. Message is : " + message);
+	}
+}
